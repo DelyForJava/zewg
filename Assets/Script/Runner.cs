f5ca2fc8f3b67ecf61bd61966586aefa99ea97a2
Dely <502270642@qq.com>
2017-06-13 17:28:26 +0800

    refactor

--- Assets/Script/Runner.cs
+++ Assets/Script/Runner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class Docker : MonoBehaviour
+public class Runner : MonoBehaviour
 {
         // Use this for initialization
         void Start()
